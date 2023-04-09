88808a839604e927143e9b9bfddafc6fea20c16f
azkifairuz <azkiajmal@gmail.com>
2023-04-10 02:57:34 +0700

    id column

--- service-media/migrations/20230409195333-create-table-media.js
+++ service-media/migrations/20230409195333-create-table-media.js
@@ -1,22 +1,22 @@
 'use strict';
 
+const { sequelize } = require('../models');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('media',
+    { 
+      id: {
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+        allowNull:false
+      },
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+     await queryInterface.dropTable('media');
   }
 };
