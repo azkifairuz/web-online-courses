cccca0d4851e2b123f108e8d4034b9b43d149058
azkifairuz <azkiajmal@gmail.com>
2023-04-09 23:28:11 +0700

    route app

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var memberRouter = require('./routes/member');
+var mediaRouter = require('./routes/media');
+var coursesRouter = require('./routes/courses');
+var ordersRouter = require('./routes/orders');
+var paymentRouter = require('./routes/payment');
 
 var app = express();
 
@@ -17,5 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/media', mediaRouter);
+app.use('/payment', paymentRouter);
+app.use('/orders', ordersRouter);
+app.use('/courses', coursesRouter);
+app.use('/member', memberRouter);
 
 module.exports = app;
