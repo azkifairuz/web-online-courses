91008e63459066ec76f2bb4fdb1b86035d16738e
azkifairuz <azkiajmal@gmail.com>
2023-04-13 01:47:17 +0700

    feat:check is image base64

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
+const isBase64 = require("is-base64")
 router.post('/',(req,res)=>{
-  res.send("tes")
+  const image = req.body.image;
+
+  if (!isBase64(image, {mimeRequired:true})) {
+    return res.status(400).json({
+      status:"error",
+      massage:"invalid image type"
+    })
+  }
+  return res.send('ok')
+
 })
 
 module.exports = router;
